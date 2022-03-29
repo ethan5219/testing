53ca3b6c5f22df62f657d57709bebde211689f32
ethan5219 <100673054+ethan5219@users.noreply.github.com>
2022-03-29 18:10:00 +0800

    newest modified

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -31,11 +31,12 @@ $routes->setAutoRoute(False);
  */
 
 
-$routes->post('/news/create', 'News::create');
+
 $routes->get('/', 'Pages::index');
 $routes->get('pages/index_hi', 'Pages::index_hi');
 $routes->get('pages/view', 'Pages::view');
 $routes->match(['get', 'post'], 'news/create', 'News::create');
+$routes->post('/news/create', 'News::create');
 $routes->get('news/(:segment)', 'News::view/$1');
 $routes->get('news', 'News::index');
 $routes->get('(:any)', 'Pages::view/$1');
